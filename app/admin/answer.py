a8ce6d797e9cc540cc91a43b58e249fa767f992a
stamaimer <stamaimer@gmail.com>
2017-07-07 15:29:48 +0800

    add filter for question id

--- app/admin/answer.py
+++ app/admin/answer.py
@@ -21,12 +21,14 @@ class AnswerModelView(AppModelView):
 
     form_excluded_columns = ["create_datetime", "update_datetime"]
 
-    column_filters = ["user.username", "question.create_datetime"]
+    column_filters = ["user.username", "question.id", "question.create_datetime"]
 
     labels = dict(user=u"用户", answer=u"答案", question=u"问题")
 
     column_labels = labels
 
     column_labels["user.username"] = u"用户"
+
+    column_labels["question.id"] = u"问题编号"
 
     column_labels["question.create_datetime"] = u"问题创建时间"
