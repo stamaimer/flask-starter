d0000486873b7e223c8397ab31173b7140e0fcff
stamaimer <stamaimer@gmail.com>
2017-12-30 21:44:24 +0800

    add project and audit

---
+++ app/model/audit.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+    app.model.audit
+    ~~~~~~~~~~~~~~~
+
+    stamaimer 12/30/17
+
+"""
+
+
+from . import db, AppModel
+
+
+class Audit(AppModel):
+
+    next_id = db.Column(db.Integer(), db.ForeignKey("audit.id"))
+
+    _next_ = db.relationship("Audit", remote_side="Audit.id", backref=db.backref("last", uselist=0), uselist=0,
+                             post_update=1, cascade="all,delete")
+
+    advice = db.Column(db.Text())
+
+    status = db.Column(db.Boolean(), default=0)
+
+    result = db.Column(db.Enum(u"同意", u"驳回"))
+
+    audit_user_id = db.Column(db.Integer(), db.ForeignKey("user.id"))
+
+    audit_user = db.relationship("User", foreign_keys=audit_user_id)
+
+    project_id = db.Column(db.Integer(), db.ForeignKey("project.id"))
+
+    project = db.relationship("Project", foreign_keys=project_id)
+    
--- app/model/project.py
+++ app/model/project.py
@@ -2,4 +2,35 @@
 
 """
 
-    
+    app.model.project
+    ~~~~~~~~~~~~~~~~~
+
+    stamaimer 12/27/17
+
+"""
+
+
+from . import db, AppModel
+
+
+class Project(AppModel):
+
+    pro_name = db.Column(db.String(128), nullable=False)
+
+    pro_time = db.Column(db.String(128), nullable=False)
+
+    pro_type = db.Column(db.String(128), nullable=False)  # 项目类别
+
+    sub_type = db.Column(db.String(128), nullable=False)  # 学科类别
+
+    res_type = db.Column(db.String(128), nullable=False)  # 研究类型
+
+    res_form = db.Column(db.String(128), nullable=False)  # 预期成果
+
+    keywords = db.Column(db.String(128), nullable=False)
+
+    status = db.Column(db.Enum(u"审批中", u"已驳回", u"已通过"), default=u"审批中")
+
+    current_audit_id = db.Column(db.Integer(), db.ForeignKey("audit.id"))
+
+    current_audit = db.relationship("Audit", foreign_keys=current_audit_id, uselist=0, cascade="all,delete", post_update=1)
